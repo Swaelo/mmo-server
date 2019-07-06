c30e4b73793a59d7e956feadce0e31a859057102
swaelo <harleylaurie@rocketmail.com>
2019-07-07 00:35:33 +1000

    moved GetSystemTime into the new Time namespace, created a new PointInTime class used to track the exact time when we last heard from a specific game client, and how much time has passed since then, used by the ConnectionManager which flags ClientConnections as dead when they havnt been heard from in a long time, then cleaned up by the GameWorld

---
+++ Server/Time/GetSystemTime.cs
@@ -0,0 +1,24 @@
+﻿// ================================================================================================================================
+// File:        GetSystemTime.cs
+// Description: Returns the current time in a nicely formatted string
+// Author:      Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using System;
+
+namespace Server.Time
+{
+    public static class GetSystemTime
+    {
+        public static string GetSystemTimeString()
+        {
+            DateTime Time = System.DateTime.Now;
+            bool PM = Time.Hour > 12;
+            int Hour = PM ? Time.Hour - 12 : Time.Hour;
+            Hour = Hour == 0 ? 12 : Hour;
+            string Minute = Time.Minute < 10 ? "0" + Time.Minute : Time.Minute.ToString();
+            string Second = Time.Second < 10 ? "0" + Time.Second : Time.Second.ToString();
+            return Hour + ":" + Minute + ":" + Second + " " + (PM ? "PM" : "AM");
+        }
+    }
+}
---
+++ Server/Time/PointInTime.cs
@@ -0,0 +1,33 @@
+﻿// ================================================================================================================================
+// File:        PointInTime.cs
+// Description: Defines a single point in time, has helper functions to check how long ago that point in time was
+// Author:	    Harley Laurie https://www.github.com/Swaelo/
+// ================================================================================================================================
+
+using System;
+using System.Collections.Generic;
+
+namespace Server.Time
+{
+    public class PointInTime
+    {
+        private DateTime BirthTime;  //Exactly when this object was created
+
+        //Default Constructor sets the member variables to track exactly when this object was created
+        public PointInTime()
+        {
+            BirthTime = DateTime.Now;
+        }
+
+        //Returns the total number of seconds that have passed since this object was created
+        public int AgeInSeconds()
+        {
+            //Get the current time
+            DateTime CurrentTime = DateTime.Now;
+            //Calculate how many seconds have passed since this objects birth and the current time value
+            int Seconds = (int)(BirthTime - CurrentTime).TotalSeconds;
+            //Return the number of seconds that have passed since the objects creation
+            return -Seconds;
+        }
+    }
+}
